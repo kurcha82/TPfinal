5faaf1d96059af049abaf2afab54d607de5816ff
Francisco Vasta <franciscovasta@gmail.com>
2021-12-23 00:03:13 -0300

    Estructura de Apps Login, Perfiles, Registro, Requeridos

---
+++ TPfinal/Requeridos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RequeridosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Requeridos'
--- TPfinal/TPfinal/settings.py
+++ TPfinal/TPfinal/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Login',
+    'Perfiles',
     'Postulantes',
+    'Registro',
+    'Requeridos',
+
+
 ]
 
 MIDDLEWARE = [
